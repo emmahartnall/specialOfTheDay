2c2280bfd2f08c940b9a3a0e6dc09be44fbeee57
Emma Hartnall <emma@hartnall.com>
2024-03-02 21:24:29 +1100

    initial commit

---
+++ src/main/java/com/emmasSpecialOfDay/specialOfTheDay/SpecialOfTheDayApplication.java
@@ -0,0 +1,13 @@
+package com.emmasSpecialOfDay.specialOfTheDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialOfTheDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialOfTheDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emmasSpecialOfDay/specialOfTheDay/SpecialOfTheDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmasSpecialOfDay.specialOfTheDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialOfTheDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
