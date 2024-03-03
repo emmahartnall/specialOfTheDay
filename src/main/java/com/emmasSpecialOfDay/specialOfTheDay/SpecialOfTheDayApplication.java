1f168bbb995a43909328663186cfea457c6a8e31
Emma Hartnall <emma@hartnall.com>
2024-03-03 21:58:40 +1100

    secondCommit

--- src/main/java/com/emmasSpecialOfDay/specialOfTheDay/SpecialOfTheDayApplication.java
+++ src/main/java/com/emmasSpecialOfDay/specialOfTheDay/SpecialOfTheDayApplication.java
@@ -3,11 +3,14 @@ package com.emmasSpecialOfDay.specialOfTheDay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.PrintStream;
+
 @SpringBootApplication
 public class SpecialOfTheDayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpecialOfTheDayApplication.class, args);
+		PrintStream printf = System.out.print("hello from the special of the day app");
+
 	}
-	//	System.out.printf("hello from the special of the day app");
-	}
+}
